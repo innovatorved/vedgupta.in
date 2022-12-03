337c0b0851ac3631290144fb7ef29f5db2834b97
Ved Gupta <vedgupta@protonmail.com>
2022-12-03 13:42:30 +0530

    analytics file url changed

--- components/metrics/Analytics.tsx
+++ components/metrics/Analytics.tsx
@@ -8,7 +8,7 @@ export default function AnalyticsCard() {
   const { data } = useSWR<Views>('/api/views', fetcher);
 
   const pageViews = new Number(data?.total);
-  const link = 'http://localhost:3000';
+  const link = 'http://nextinnovate.tech';
 
   return (
     <MetricCard
