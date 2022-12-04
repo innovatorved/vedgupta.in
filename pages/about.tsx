0a73c6937481cdec14b4ad44e58ef56895b19899
Ved Gupta <vedgupta@protonmail.com>
2022-12-05 00:39:00 +0530

    updated

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,7 +7,7 @@ import avatarBW from 'public/avatar-bw.jpg';
 
 export default function About() {
   return (
-    <Container title="About – Lee Robinson">
+    <Container title="About – Ved Gupta">
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           About Me
