576cb7b05dae881cd0538b7fab3a1d69e1ebb3c3
Ved Gupta <vedgupta@protonmail.com>
2022-12-05 18:36:41 +0530

    revalidate added

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -81,5 +81,5 @@ export default function Blog({
 export async function getStaticProps({ preview = false }) {
   const posts: Post[] = await getClient(preview).fetch(indexQuery);
 
-  return { props: { posts } };
+  return { props: { posts } , revalidate: 60};
 }
