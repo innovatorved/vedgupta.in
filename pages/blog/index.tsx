de519b1538f2ab7ee8db7b7ca39a64f4ffbbb7b4
Ved Gupta <vedgupta@protonmail.com>
2022-12-08 12:48:09 +0530

    heading changed

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -56,9 +56,9 @@ export default function Blog({
         </div>
         {!searchValue && <></>}
         <Suspense fallback={null}>
-          <h3 className="mt-8 mb-6 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
+          <h2 className="mt-8 mb-6 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
             All Posts
-          </h3>
+          </h2>
           {!filteredBlogPosts.length && (
             <p className="mb-4 text-gray-600 dark:text-gray-400">
               No posts found.
