bf7c8ecd807883d912fbbcf44bc754790ebf0362
Ved Gupta <vedgupta@protonmail.com>
2023-07-30 16:44:15 +0530

    BUG resolved #1

--- middleware.ts
+++ middleware.ts
@@ -4,3 +4,8 @@ import { redirectIfNotLogin } from 'middlewares/redirectIfNotLogin';
 const middlewares = [redirectIfNotLogin];
 
 export default stackMiddlewares(middlewares);
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: '/api/:path*',
+}
