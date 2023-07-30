317b08fb6b45f916aee65b8a998c717056095c1f
Ved Gupta <vedgupta@protonmail.com>
2023-07-30 22:51:39 +0530

    table

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -99,7 +99,7 @@ export default function DataTable({
             type="text"
             onChange={handleSearch}
             placeholder="Search database"
-            className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-200 rounded-md dark:border-gray-900  focus:border-y-stone-600 dark:bg-gray-800 dark:text-gray-100"
+            className="block w-full px-4 py-2 text-gray-900 border border-gray-200 rounded-md dark:border-gray-900  focus:border-y-stone-600 dark:bg-gray-800 dark:text-gray-100"
           />
           <svg
             className="absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"
