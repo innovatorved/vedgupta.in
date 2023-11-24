8eeb8cf1853d72fe0c3670bda93d04f59f93ab74
Ved Gupta <vedgupta@protonmail.com>
2023-11-24 23:31:17 +0530

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -53,6 +53,17 @@ export default function Document(props) {
           content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
           name="robots"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NR39J8J');
+            `,
+          }}
+        />
         <Partytown debug={true} forward={['dataLayer.push']} />
       </Head>
       <body className="bg-white dark:bg-black text-white dark:text-black">
